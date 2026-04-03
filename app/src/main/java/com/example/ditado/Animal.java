acfc26edb20e6344ba2320d198c7f02ac249b824
Giovanna <giovanna_teodoro@ufms.br>
2026-04-03 13:29:20 -0400

    Imagens Animais

---
+++ app/src/main/java/com/example/ditado/Animal.java
@@ -0,0 +1,17 @@
+package com.example.ditado;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Animal{
+    private String nome;
+    private int imagemId; // ref para res/drawable
+    private int audioId; // ref para res/raw
+
+    public Animal(String nome, int imagemId, int audioId){
+        this.nome = nome;
+        this.imagemId = imagemId;
+        this.audioId = audioId;
+    }
+}
+
