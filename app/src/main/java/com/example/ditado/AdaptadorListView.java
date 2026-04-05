6a0edbce02fcff7ae34e48ebaf3ee9e18849ae60
Daniely Marcon <daniellymaximo13@gmail.com>
2026-04-05 01:52:26 -0400

    feat:Adicionando listView personalizado, fab e ação de itens do listview

---
+++ app/src/main/java/com/example/ditado/AdaptadorListView.java
@@ -0,0 +1,46 @@
+package com.example.ditado;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+
+public class AdaptadorListView extends ArrayAdapter<Animal> {
+
+    public AdaptadorListView(Context context, List<Animal> listaAnimais) {
+                super(context, R.layout.item_lista_animal, listaAnimais);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.item_lista_animal, parent, false);
+        }
+
+
+        Animal animalAtual = getItem(position);
+
+
+        ImageView foto = convertView.findViewById(R.id.imgFotoLinha);
+        TextView nome = convertView.findViewById(R.id.txtNomeLinha);
+
+        if (animalAtual != null) {
+            foto.setImageResource(animalAtual.getImagemId()); // Ajuste se seu getter for diferente
+            nome.setText(animalAtual.getNome());
+        }
+
+        return convertView;
+    }
+}
--- app/src/main/java/com/example/ditado/TerceiroFragment.java
+++ app/src/main/java/com/example/ditado/TerceiroFragment.java
@@ -1,64 +1,57 @@
 package com.example.ditado;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link TerceiroFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.ditado.databinding.FragmentFirstBinding;
+import com.example.ditado.databinding.FragmentSecondBinding;
+import com.example.ditado.databinding.FragmentTerceiroBinding;
+
+import java.util.ArrayList;
+
+
 public class TerceiroFragment extends Fragment {
+    private FragmentTerceiroBinding binding;
+    private MediaPlayer mediaPlayer;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public TerceiroFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment TerceiroFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static TerceiroFragment newInstance(String param1, String param2) {
-        TerceiroFragment fragment = new TerceiroFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
-    @Override
+     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_terceiro, container, false);
+        binding = FragmentTerceiroBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ArrayList<Animal> aprendidos = (ArrayList<Animal>) getArguments().getSerializable("aprendidos");
+        AdaptadorListView meuAdaptador = new AdaptadorListView(getContext(), aprendidos);
+        binding.lvEstatisticas.setAdapter(meuAdaptador);
+
+
+        binding.lvEstatisticas.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                mediaPlayer = MediaPlayer.create(getContext(), aprendidos.get(position).getAudioId());
+                mediaPlayer.start();
+            }
+        });
+        binding.fabF3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                aprendidos.clear();
+                meuAdaptador.notifyDataSetChanged();
+            }
+        });
     }
 }
