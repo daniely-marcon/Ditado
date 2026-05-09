146e6230d486592b7d2d20da03e01974cf4ea66f
Giovanna <giovanna_teodoro@ufms.br>
2026-05-09 13:36:23 -0400

    Tela de envio de código

--- app/src/main/java/com/example/ditado/CadastroAnimalFragment.java
+++ app/src/main/java/com/example/ditado/CadastroAnimalFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link CadastroAnimalActivity#newInstance} factory method to
+ * Use the {@link CadastroAnimalFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class CadastroAnimalActivity extends Fragment {
+public class CadastroAnimalFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class CadastroAnimalActivity extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public CadastroAnimalActivity() {
+    public CadastroAnimalFragment() {
         // Required empty public constructor
     }
 
@@ -37,8 +37,8 @@ public class CadastroAnimalActivity extends Fragment {
      * @return A new instance of fragment CadastroAnimalActivity.
      */
     // TODO: Rename and change types and number of parameters
-    public static CadastroAnimalActivity newInstance(String param1, String param2) {
-        CadastroAnimalActivity fragment = new CadastroAnimalActivity();
+    public static CadastroAnimalFragment newInstance(String param1, String param2) {
+        CadastroAnimalFragment fragment = new CadastroAnimalFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
