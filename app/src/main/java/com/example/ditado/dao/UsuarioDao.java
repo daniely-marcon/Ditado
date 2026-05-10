77a9c2aba275abe443fffe8dae5330bbfac2fe87
Giovanna <giovanna_teodoro@ufms.br>
2026-05-10 17:41:09 -0400

    ,

--- app/src/main/java/com/example/ditado/dao/UsuarioDao.java
+++ app/src/main/java/com/example/ditado/dao/UsuarioDao.java
@@ -24,6 +24,9 @@ public interface UsuarioDao {
     @Query("SELECT * FROM Usuario WHERE email= :email")
     Usuario buscarUsuario(String email);
 
+    @Query("UPDATE senha SET senha = :novaSenha WHERE email = :emailUser")
+    fun atualizarSenha(String emailUser, String novaSenha);
+
 
     @Insert
     void insert(Usuario usuario);
--- app/src/main/java/com/example/ditado/entities/Usuario.java
+++ app/src/main/java/com/example/ditado/entities/Usuario.java
@@ -60,4 +60,7 @@ public class Usuario {
     public void setSenha(String number) {
 
     }
+
+    public void atualizarSenha(String novaSenha, Usuario usuario) {
+    }
 }
